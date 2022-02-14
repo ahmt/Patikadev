725b6e2e448340050dc7eb465f5d96cc97f7f514
ahmt <78702998+ahmt@users.noreply.github.com>
2022-02-14 17:12:18 +0300

    java eklendi

--- react/odev2/src/App.js
+++ react/odev2/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import list from './components/list';
+import listDiv from './components/listDiv';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+     
     </div>
   );
 }
